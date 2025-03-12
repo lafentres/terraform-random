242f8f25e7941c5afbd184db2efeba9c28502596
Krista LaFentres (she/her) <lafentres@users.noreply.github.com>
2025-03-12 16:22:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,20 @@
+variable "foo_test" {
+  type = string
+}
+
 resource "random_id" "random" {
   keepers = {
     uuid = uuid()
   }
 
   byte_length = 12
-
-  provisioner "local-exec" {
-    command = "echo $FOO"
-  }
  
 }
 
 output "random" {
   value = random_id.random.hex
+}
+
+output "foo-test-str" {
+  value = var.foo_test
 }
